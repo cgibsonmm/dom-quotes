abaf322fa6d1893eb4c0bca50bffd91b54bb19cd
Jason Seminara <jseminara@gmail.com>
2017-07-27 09:42:12 -0400

    integrated vanillajs prompt into master

---
+++ js/vanilla-solution.js
@@ -0,0 +1,28 @@
+// vanilla-solution.js
+/* eslint no-unused-vars: 0 */
+/* eslint-env browser, node */
+
+// TODO: [1] Select the <body>
+const body = document.querySelector('body');
+
+// TODO: [2] Select the <header>
+const header = null;
+
+// TODO: [3] Select all of the elements with class "quote"
+const quotes = null;
+
+// TODO: [4] Select all of the subject elements
+const subjects = null;
+
+// TODO: [5] Select the navigation links
+const navs = null;
+
+// TODO: [6] Select all the quotes in the "life" section.
+const lifeQuotes = null;
+
+// TODO: [7] Using relative selection, select the <p> element containing the second Mark Twain quote
+const markTwain = null;
+
+// TODO: [8] Do the same using a query selector
+const markTwainQS = null;
+
